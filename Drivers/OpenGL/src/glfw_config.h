3cda8d1422304fc5653c2bb6cbe6df314c5234bf
gianbartadougall <giavanniavila@gmail.com>
2022-11-20 14:18:49 +1000

    Updated the MakeFile in the 'Drivers' folder and the 'GSandBox' folder. GSandBox succesfully compiles and creates a GLFW window.

--- Drivers/OpenGL/src/glfw_config.h
+++ Drivers/OpenGL/src/glfw_config.h
@@ -3,7 +3,4 @@
 
 #define _GLFW_WIN32 // Must be in internal.h
 
-#define _GLFW_WGL // Not sure where
-#define _GLFW_USE_OPENGL // Not sure where
-
 #endif // GLFW_CONFIG_H
--- GSandBox/Main/Src/game.cpp
+++ GSandBox/Main/Src/game.cpp
@@ -4,14 +4,14 @@
  * @brief System file for game
  * @version 0.1
  * @date --
- * 
- * @copyright Copyright (c) 
- * 
+ *
+ * @copyright Copyright (c)
+ *
  */
 /* Public Includes */
 #include <iostream>
 #include <chrono> // Required for getting system time
-// #include <ctime>    
+// #include <ctime>
 
 /* Private Includes */
 #include "game.h"
@@ -27,51 +27,62 @@ using namespace std::chrono;
 GLFWwindow* window;
 
 /* Private Function Declartations */
+void error_callback(int error, const char* description);
 
 Game::Game() {
     // Create the window for the game
     // window = glfwCreateWindow(640, 480, "Sequoia", glfwGetPrimaryMonitor(), NULL);
 }
 
-Game::~Game() {
-    glfwDestroyWindow(window);
-}
+Game::~Game() {}
 
 void Game::run() {
 
-    if(!glfwInit()) {
+    if (glfwInit() != GLFW_TRUE) {
         std::cout << "GLFW could not be initialised" << std::endl;
         return;
     }
 
-    window = glfwCreateWindow(640, 480, "Sequoia", glfwGetPrimaryMonitor(), NULL);
-
+    window = glfwCreateWindow(1920, 1080, "Sequoia", NULL, NULL);
     if (window == NULL) {
         std::cout << "Window could not be created" << std::endl;
-    
-        glfwDestroyWindow(window); 
+        glfwTerminate();
         return;
     }
 
-    // Creating a new matrix testing
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-    int frames = 0;
+    int frames                     = 0;
     system_clock::time_point time1 = system_clock::now();
 
+    // Run main game loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
 
-    // Create a game loop
-    while (1) {
+        /* Render here */
+        render();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
 
         // Print the FPS
         auto elapsedTime = duration_cast<milliseconds>(system_clock::now() - time1);
         if (elapsedTime.count() >= 1000) {
             std::cout << "FPS: " << frames << std::endl;
             frames = 0;
-            time1 = system_clock::now();
+            time1  = system_clock::now();
         }
 
         frames++;
     }
 
+    glfwTerminate();
+}
 
+void Game::render() {
+    glClearColor(0.6f, 0.6f, 0.2f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
