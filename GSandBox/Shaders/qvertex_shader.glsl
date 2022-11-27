38c943281470b22b6a4ca2a520f1f87ba8687d95
gianbartadougall <giavanniavila@gmail.com>
2022-11-27 18:42:22 +1000

    Cleaned up the vertex shader to be more readable

--- GSandBox/Shaders/qvertex_shader.glsl
+++ GSandBox/Shaders/qvertex_shader.glsl
@@ -7,23 +7,22 @@ uniform vec3 translation;
 uniform vec3 scale;
 uniform vec4 rotate;
 
-uniform mat4 transformation;
-uniform float theta;
-uniform mat4 projectionMatrix;
-
 out vec3 Colour;
 
 void main() {
-    vec3 r = rotate.yzw;
-    float norm = 1/length(r);
 
-    // Calculate the unit vector for the axis of rotation 
-    vec3 u = vec3(norm * r.x, norm * r.y, norm * r.z);
-    
+    // Translate and scale position
+    vec3 pos = position + translation;
+    pos = vec3(scale.x * pos.x, scale.y * pos.y, scale.z * pos.z);
+
+    // Rotate position. Note rotate is a quaternion => rotate.x = angle and rotate.yzw = axis of rotation
     float theta = rotate.x;
-    vec3 pos = (cos(theta) * position) + ((1 - cos(theta)) * dot(u, position) * u) + (sin(theta) * cross(u, position));
-    gl_Position = vec4(pos, 1.0f);
-    vec4 pos1 = (vec4(pos, 1.0) + vec4(translation, 0));
-    gl_Position = vec4(scale.x * pos1.x, scale.y * pos1.y, scale.z * pos1.z, pos1.w);
-    Colour = vec3(pos1.xyz);
+    vec3 rotAxis = 1/length(rotate.yzw) * rotate.yzw;
+
+    // Equation for rotating a point around and axis using quaternions
+    pos = (cos(theta) * pos) + ((1 - cos(theta)) * dot(rotAxis, pos) * rotAxis) + (sin(theta) * cross(rotAxis, pos));
+    
+    // Assign the position and the colour
+    gl_Position = vec4(pos, 1.0);
+    Colour = vec3(pos.xyz);
 }
